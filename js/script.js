e5cc341e805647c048c603e6f6e41b2b5961ccfc
lenahlawi07 <lynnhalawi07@gmail.com>
2026-05-04 09:48:46 -0300

    feat : Exemplos basicos de JS, declaracoes.

--- js/script.js
+++ js/script.js
@@ -3,3 +3,15 @@
 
 // alert("Meu nome é " + nome);
 // console.log("Meu nome é " + nome);
+
+//declarando variaveis em JS com let
+
+let nome = "Jose";
+let idade = 33;
+let status = false;
+
+//imprimir os dados do nosso usuario JOSE com console.log usando concatenacao simples:
+console.log(nome + " de " + idade +" anos esta " + (status === true ? "empregado" : "desempregado") + " atualmente.")
+
+//exercicio
+console.log(`${nome} de ${idade} anos esta ${(status === true ? "empregado" : "desempregado")} atualmente.`);
