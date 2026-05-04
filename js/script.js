0a878a4d3bf761b5c1d94e1c9665f8eb197bc766
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2026-05-04 09:48:34 -0300

    feat : Exemplos básicos de JS, declarações.

--- js/script.js
+++ js/script.js
@@ -3,3 +3,31 @@
 
 // alert("Meu nome é " + nome);
 // console.log("Meu nome é " + nome);
+
+// HOISTING
+// var nome = "Alê";
+// let sobreNome = "Carlos";
+
+// if(true){
+
+//     var nome = "Joaquim";
+//     let sobreNome = "Das Couves";
+
+// }
+
+// console.log(nome);
+// console.log(sobreNome);
+
+
+
+//Declarando variáveis em JS com let
+
+let nome   = "José";
+let idade  = 33;
+let status = false;
+
+//Imprimir os dados do nosso usuário JOSÉ com console.log usando concatenação simples:
+console.log(nome + " de " + idade +" anos está " + (status === true ? "empregado" : "desempregado") + " atualmente.");
+
+//Exercício: agora escreva a mesma frase com template-literal, onde o texto e as variáveis ficam dentro de ` ` e as variáveis são apresentadas entre os símbolos de ${variável}.
+console.log( `${nome} de ${idade} anos está ${(status === true ? "empregado" : "desempregado")} atualmente.` );
