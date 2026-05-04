e112709e90f4d818c327a1ed569ed72c67ce2ee3
arthurarrudaa <arthur.arruda.dev@gmail.com>
2026-05-04 09:50:46 -0300

    feat : atualizações

--- js/script.js
+++ js/script.js
@@ -5,3 +5,11 @@
 // console.log("Meu nome é " + nome);
 
 document.write("Olá Mundo!");
+
+let nome = "José";
+let idade = 12;
+let status = false;
+
+console.log(nome + " de " + idade + " anos de idade está " + (status === true ? "empregado" : "desempregado") + " atualmente. ");
+
+console.log(`${nome} de ${idade} anos está ${(status === true ? "empregado" : "desempregado")} atualmente.` );
