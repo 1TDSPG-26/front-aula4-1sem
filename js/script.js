237c7dd7ffc989e95e103da0ff100068d4483898
Rikk <tarikmoussa90@gmail.com>
2026-05-04 09:18:22 -0300

    feat: js externo

--- js/script.js
+++ js/script.js
@@ -3,3 +3,14 @@
 
 // alert("Meu nome é " + nome);
 // console.log("Meu nome é " + nome);
+
+var nome = "Alê";
+let sobreNome = "Carlos";
+
+if(true){
+     nome = "Joaquim";
+     sobreNome = "Das Couves";
+}
+
+console.log(nome);
+console.log(sobreNome);
