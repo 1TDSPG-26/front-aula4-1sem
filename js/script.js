388e598944c157362a635dd479ae10ad3c494c4e
giovannidevazevedo <giovanniazevedo07@gmail.com>
2026-05-04 09:50:24 -0300

    Feat : explorando JS externo console

--- js/script.js
+++ js/script.js
@@ -3,3 +3,8 @@
 
 // alert("Meu nome é " + nome);
 // console.log("Meu nome é " + nome);
+let nome = "giovanni"
+let status = false
+let idade = 19
+
+console.log(`${nome} de ${idade} anos esta ${(status === true ? "empregado": "desenpregado")} atualmente`)
