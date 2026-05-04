b4517b2eaaa42a30e0941192f8cf081eae3c927d
Pedro-H-Salvatore <pedrosalvatore2112@gmail.com>
2026-05-04 09:49:47 -0300

    feat:  Declarando e imprimindo vaáriaveis no console a partir do "${váriavel}"

--- js/script.js
+++ js/script.js
@@ -3,3 +3,14 @@
 
 // alert("Meu nome é " + nome);
 // console.log("Meu nome é " + nome);
+ 
+//Declarando Variaveis 
+
+let nome ="josé";
+let idade = 33;
+let status = false;
+
+//Imprimir dados do usuário com console.log, usando contatenação simples
+console.log(`${nome} de ${idade} anos está ${(status === true ? " empregado" : "desempregado")} atualmente` );
+
+
