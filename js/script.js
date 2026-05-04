1d9c91ce5e24feb7b120b5201f6824a4c0260647
manuelalramos <manuela.lima.r@outlook.com>
2026-05-04 09:48:51 -0300

    feat : Exemplos de declaração em JS.

--- js/script.js
+++ js/script.js
@@ -3,3 +3,15 @@
 
 // alert("Meu nome é " + nome);
 // console.log("Meu nome é " + nome);
+
+//Declarando variáveis em JS com let
+
+let nome = "José";
+let idade = 33;
+let status = false;
+
+//Imprimindo os dados do noss usário José com console.log usando concatenação simples>
+
+console.log(nome + " tem " + idade + " anos. Está " +(status === true ? "empregado " : "desempregado " + "atualmente."))
+
+console.log(`${nome} tem ${idade} está ${status === true ? "empregado" : "desempregado"} atualmente. Template-literal`);
