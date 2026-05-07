d32f64553b30de38ed59124484b977d8672d4fd7
RafaelSantos56 <rafarbcosta17@gmail.com>
2026-05-07 09:00:09 -0300

    feat: form html criado semanticamente

--- js/script.js
+++ js/script.js
@@ -1,50 +1,50 @@
-// alert("olá mundo!");
-// var nome = prompt("Olá digite seu nome");
+// // alert("olá mundo!");
+// // var nome = prompt("Olá digite seu nome");
 
-// alert("Meu nome é " + nome);
-// console.log("Meu nome é " + nome);
+// // alert("Meu nome é " + nome);
+// // console.log("Meu nome é " + nome);
 
-// HOISTING
-// var nome = "Alê";
-// let sobreNome = "Carlos";
+// // HOISTING
+// // var nome = "Alê";
+// // let sobreNome = "Carlos";
 
-// if(true){
+// // if(true){
 
-//     var nome = "Joaquim";
-//     let sobreNome = "Das Couves";
+// //     var nome = "Joaquim";
+// //     let sobreNome = "Das Couves";
 
-// }
+// // }
 
-// console.log(nome);
-// console.log(sobreNome);
-
-
-
-//Declarando variaveis em JS com let
-
-let nome = "José";
-let idade = 33;
-let status = false;
-
-//imprimir os dados do nosso usuário JOSÉ com console.log usando concatenação simples;
-console.log(nome +  " de "  + idade +"anos está " + (status === true ? "empregado" : "desempregado") + " atualmente.");
-
-//Exercício, agora escreva a mesma frase com template-literal, onde o texto e as variáveis ficam dentro de ` ` e as variáveis são apresentadas entre os símbolos de ${variável}.
-console.log(`${nome} de ${idade} anos está ${(status === true ? "empregado" : "desempregado")} atualmente.`);
+// // console.log(nome);
+// // console.log(sobreNome);
 
 
 
-// alert("Meu nome é " + nome);
-// console.log("Meu nome é " + nome);
+// //Declarando variaveis em JS com let
 
-//Declarando variáveis em JS com let
+// let nome = "José";
+// let idade = 33;
+// let status = false;
 
-let nome = "José";
-let idade = 33;
-let status = false;
+// //imprimir os dados do nosso usuário JOSÉ com console.log usando concatenação simples;
+// console.log(nome +  " de "  + idade +"anos está " + (status === true ? "empregado" : "desempregado") + " atualmente.");
 
-//Imprimindo os dados do noss usário José com console.log usando concatenação simples>
+// //Exercício, agora escreva a mesma frase com template-literal, onde o texto e as variáveis ficam dentro de ` ` e as variáveis são apresentadas entre os símbolos de ${variável}.
+// console.log(`${nome} de ${idade} anos está ${(status === true ? "empregado" : "desempregado")} atualmente.`);
 
-console.log(nome + " tem " + idade + " anos. Está " +(status === true ? "empregado " : "desempregado " + "atualmente."))
 
-console.log(`${nome} tem ${idade} está ${status === true ? "empregado" : "desempregado"} atualmente. Template-literal`);
+
+// // alert("Meu nome é " + nome);
+// // console.log("Meu nome é " + nome);
+
+// //Declarando variáveis em JS com let
+
+// let nome = "José";
+// let idade = 33;
+// let status = false;
+
+// //Imprimindo os dados do noss usário José com console.log usando concatenação simples>
+
+// console.log(nome + " tem " + idade + " anos. Está " +(status === true ? "empregado " : "desempregado " + "atualmente."))
+
+// console.log(`${nome} tem ${idade} está ${status === true ? "empregado" : "desempregado"} atualmente. Template-literal`);
