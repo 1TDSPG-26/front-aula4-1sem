a909d8f9bfe3c9175c332589155c6566c52df0eb
yasminmartins18 <yasmin.s.s.martins1804@gmail.com>
2026-05-07 09:51:48 -0300

    Feat: Manipulação do DOM e posição da referencia do JS na página HTML.

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// capturando o botão de entrar
+const botaoEntrar = document.getElementById("btnEntrar");
+
+//atrelando ao botão uma função que vai receber dos parâmetros
+// o primeiro será o evento de tela, o segundo será a função anônima ou a funçaõ de callBack
+botaoEntrar.addEventListener("evento", function(){
+    console.log("botão de entrar clicando");
+    
+});
