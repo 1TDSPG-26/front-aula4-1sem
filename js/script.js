39f478e32b8fc8b01b8f8bbbc1a379704bc74363
RafaelSantos56 <rafarbcosta17@gmail.com>
2026-05-07 09:48:03 -0300

    feat: arrumando o login e o botao entrar com jv

--- js/script.js
+++ js/script.js
@@ -1,50 +1,11 @@
-// // alert("olá mundo!");
-// // var nome = prompt("Olá digite seu nome");
 
-// // alert("Meu nome é " + nome);
-// // console.log("Meu nome é " + nome);
+// capturando botao de entrar
+const botaoEntrar = document.getElementById("btnEntrar");
 
-// // HOISTING
-// // var nome = "Alê";
-// // let sobreNome = "Carlos";
+console.log(botaoEntrar)
 
-// // if(true){
-
-// //     var nome = "Joaquim";
-// //     let sobreNome = "Das Couves";
-
-// // }
-
-// // console.log(nome);
-// // console.log(sobreNome);
-
-
-
-// //Declarando variaveis em JS com let
-
-// let nome = "José";
-// let idade = 33;
-// let status = false;
-
-// //imprimir os dados do nosso usuário JOSÉ com console.log usando concatenação simples;
-// console.log(nome +  " de "  + idade +"anos está " + (status === true ? "empregado" : "desempregado") + " atualmente.");
-
-// //Exercício, agora escreva a mesma frase com template-literal, onde o texto e as variáveis ficam dentro de ` ` e as variáveis são apresentadas entre os símbolos de ${variável}.
-// console.log(`${nome} de ${idade} anos está ${(status === true ? "empregado" : "desempregado")} atualmente.`);
-
-
-
-// // alert("Meu nome é " + nome);
-// // console.log("Meu nome é " + nome);
-
-// //Declarando variáveis em JS com let
-
-// let nome = "José";
-// let idade = 33;
-// let status = false;
-
-// //Imprimindo os dados do noss usário José com console.log usando concatenação simples>
-
-// console.log(nome + " tem " + idade + " anos. Está " +(status === true ? "empregado " : "desempregado " + "atualmente."))
-
-// console.log(`${nome} tem ${idade} está ${status === true ? "empregado" : "desempregado"} atualmente. Template-literal`);
+//Atrelando ao botao uma função que vai receber dois parâmetros.
+//O primeiro será o evento de tela, o segundo será a função de callBack ou função anonima.
+botaoEntrar.addEventListener("click", function(){
+    console.log("Botão de entrar clicado")
+});
