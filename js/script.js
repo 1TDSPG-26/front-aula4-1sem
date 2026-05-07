87a4c659c8075e35451634318f0d04515d254484
Fabricio Aquiles <fab.aquiles123@gmail.com>
2026-05-07 08:12:13 -0300

    Merge

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
 alert("olá mundo!");
 var nome = prompt("Olá digite seu nome");
 
-alert("Meu nome é " + nome);
-console.log("Meu nome é " + nome);
+// alert("Meu nome é " + nome);
+// console.log("Meu nome é " + nome);
+
+//Declarando variáveis em JS com let
+
+let nome = "José";
+let idade = 33;
+let status = false;
+
+//Imprimindo os dados do noss usário José com console.log usando concatenação simples>
+
+console.log(nome + " tem " + idade + " anos. Está " +(status === true ? "empregado " : "desempregado " + "atualmente."))
+
+console.log(`${nome} tem ${idade} está ${status === true ? "empregado" : "desempregado"} atualmente. Template-literal`);
