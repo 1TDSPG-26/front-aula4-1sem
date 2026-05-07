b7f81a6d930905f51aca46625f2e22cfbcc8538a
miguelsilva71 <miguel.sil010@gmail.com>
2026-05-07 09:52:22 -0300

    Feat: ultilizando js para o botâo

--- js/script.js
+++ js/script.js
@@ -1,47 +1,10 @@
-// alert("olá mundo!");
-// var nome = prompt("Olá digite seu nome");
 
-// alert("Meu nome é " + nome);
-// console.log("Meu nome é " + nome);
+//Capturando o botâo de entrar
+const botaoEntrar = document.getElementById("btnEntrar");
+console.log(botaoEntrar);
 
-<<<<<<< HEAD
-// HOISTING
-// var nome = "Alê";
-// let sobreNome = "Carlos";
-
-// if(true){
-
-//     var nome = "Joaquim";
-//     let sobreNome = "Das Couves";
-
-// }
-
-// console.log(nome);
-// console.log(sobreNome);
-
-
-
-//Declarando variáveis em JS com let
-
-let nome   = "José";
-let idade  = 33;
-let status = false;
-
-//Imprimir os dados do nosso usuário JOSÉ com console.log usando concatenação simples:
-console.log(nome + " de " + idade +" anos está " + (status === true ? "empregado" : "desempregado") + " atualmente.");
-
-//Exercício: agora escreva a mesma frase com template-literal, onde o texto e as variáveis ficam dentro de ` ` e as variáveis são apresentadas entre os símbolos de ${variável}.
-console.log( `${nome} de ${idade} anos está ${(status === true ? "empregado" : "desempregado")} atualmente.` ); 
-=======
-//Declarando variáveis em JS com let
-
-let nome = "José";
-let idade = 33;
-let status = false;
-
-//Imprimindo os dados do noss usário José com console.log usando concatenação simples>
-
-console.log(nome + " tem " + idade + " anos. Está " +(status === true ? "empregado " : "desempregado " + "atualmente."))
-
-console.log(`${nome} tem ${idade} está ${status === true ? "empregado" : "desempregado"} atualmente. Template-literal`);
->>>>>>> origin/rm572956
+//Atrelando ao botao uma função que vai receber dois parâmetros.
+//O primeiro será o evento de tela, o segundo será a função de callback ou função anônima.
+botaoEntrar.addEventListener("click",function(){
+    console.log("Botão de entrar clicado");
+});
