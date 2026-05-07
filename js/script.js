2e9e31a831a10480deaa1c1050c7f8c78a362632
miguelsilva71 <miguel.sil010@gmail.com>
2026-05-07 12:43:22 -0300

    feat: ajustando js email e senha

--- js/script.js
+++ js/script.js
@@ -1,10 +1,31 @@
 
-//Capturando o botâo de entrar
+//Capturando o botão de entrar
 const botaoEntrar = document.getElementById("btnEntrar");
+
 console.log(botaoEntrar);
 
-//Atrelando ao botao uma função que vai receber dois parâmetros.
-//O primeiro será o evento de tela, o segundo será a função de callback ou função anônima.
-botaoEntrar.addEventListener("click",function(){
+
+//Atrelando ao botão uma função que vai receber dois parâmetros.
+// O primeiro será o evento de tela, o segundo será a função de callBack ou função anônima.
+botaoEntrar.addEventListener("click", function() {
+
     console.log("Botão de entrar clicado");
+
+ const email = document.getElementById("idEmail").value;
+ console.log(email)
+ console.log("Email clicado")
+
+ const senha = document.getElementById("idSenha").value;
+ console.log(senha)
+ console.log("Senha clicada")
+
+
+    //Para casa:
+    //Capturar os campos de email e senha.
+    //Armazenar em variáveis.
+    //Imprimir seus valores.
+
+
+
+
 });
