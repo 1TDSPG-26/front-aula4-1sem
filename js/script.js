500de7b7c78ce7ec378f774fc3489d79eeb243f4
Tailyni.dev <tailyni80@gmail.com>
2026-05-11 11:08:50 -0300

    feat;atualização script

--- js/script.js
+++ js/script.js
@@ -1,33 +1,50 @@
-// alert("olá mundo!");
-// var nome = prompt("Olá digite seu nome");
+// // alert("olá mundo!");
+// // var nome = prompt("Olá digite seu nome");
 
-// alert("Meu nome é " + nome);
-// console.log("Meu nome é " + nome);
+// // alert("Meu nome é " + nome);
+// // console.log("Meu nome é " + nome);
 
-// HOISTING
-// var nome = "Alê";
-// let sobreNome = "Carlos";
+// // HOISTING
+// // var nome = "Alê";
+// // let sobreNome = "Carlos";
 
-// if(true){
+// // if(true){
 
-//     var nome = "Joaquim";
-//     let sobreNome = "Das Couves";
+// //     var nome = "Joaquim";
+// //     let sobreNome = "Das Couves";
 
-// }
+// // }
 
-// console.log(nome);
-// console.log(sobreNome);
+// // console.log(nome);
+// // console.log(sobreNome);
 
 
 
-//Declarando variáveis em JS com let
+// //Declarando variáveis em JS com let
 
-let nome   = "José";
-let idade  = 33;
-let status = false;
+// let nome   = "José";
+// let idade  = 33;
+// let status = false;
 
-//Imprimir os dados do nosso usuário JOSÉ com console.log usando concatenação simples:
-console.log(nome + " de " + idade +" anos está " + (status === true ? "empregado" : "desempregado") + " atualmente.");
+// //Imprimir os dados do nosso usuário JOSÉ com console.log usando concatenação simples:
+// console.log(nome + " de " + idade +" anos está " + (status === true ? "empregado" : "desempregado") + " atualmente.");
 
-//Exercício: agora escreva a mesma frase com template-literal, onde o texto e as variáveis ficam dentro de ` ` e as variáveis são apresentadas entre os símbolos de ${variável}.
-console.log( `${nome} de ${idade} anos está ${(status === true ? "empregado" : "desempregado")} atualmente.` );
+// //Exercício: agora escreva a mesma frase com template-literal, onde o texto e as variáveis ficam dentro de ` ` e as variáveis são apresentadas entre os símbolos de ${variável}.
+// console.log( `${nome} de ${idade} anos está ${(status === true ? "empregado" : "desempregado")} atualmente.` );
+
+
+
+
+
+
+btnEntrar.addEventlistener("click", function(){
+    const email = document.getElementById("idEmail")
+    const senha = document.getElementById("idsenha")
+    
+    
+    console.log("Email : " + email.value + "e senha: " + senha.value)
+    
+    console.log(`Email : ${email.value} e senha: ${senha.value}.`)
+
+    //
+})
